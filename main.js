f8a9f20553580fa944e3e84badbec2c06b19b4d6
overflow326 <s.daizou@outlook.com>
2019-02-15 21:49:46 +0900

    test

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const lastname = `shiraishi`
-const firstname = `daizou`
-const fullname = lastname + ` ` + firstname
+const lastname = "shiraishi"
+const firstname = "daizou"
+const fullname = lastname + " " + firstname
 const myAge = 25
-const friendAge = 53
+const friendAge = 23
 const totalAge = myAge + friendAge
 const numberOfDays = totalAge * 365
 const numberOfChildren = 3
 const numberOfCakes = 10
 const numberOfCakesByChild = numberOfCakes / numberOfChildren
 const numberOfRestOfCakes = numberOfCakes % numberOfChildren
+
+console.log(lastname);
+console.log(firstname);
+console.log(fullname);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOfDays);
+console.log(numberOfChildren);
+console.log(numberOfCakes);
+console.log(numberOfCakesByChild);
+console.log(numberOfRestOfCakes);
